280022119e2216653c924487f90894767ce9458f
Marius Wachtler <undingen@gmail.com>
2017-01-31 14:33:39 +0000

    f

--- percona_playback/plugin.cmake
+++ percona_playback/plugin.cmake
@@ -1,10 +1,18 @@
+function(read_ini_key ini_text key outputvar)
+ STRING(REGEX REPLACE "(.*)${key}=([^\n]+)(.*)" "\\2" outputvar "${ini_text}")
+endfunction(read_ini_key)
+
 function(read_plugin_ini plugin_name)
  file(STRINGS plugin.ini ini_text NEWLINE_CONSUME)
 
- STRING(REGEX REPLACE "(.*)title=([^\n]+)(.*)" "\\2" plugin_title "${ini_text}")
- STRING(REGEX REPLACE "(.*)description=([^\n]+)(.*)" "\\2" description "${ini_text}")
- STRING(REGEX REPLACE "(.*)version=([^\n]+)(.*)" "\\2" plugin_version "${ini_text}")
- STRING(REGEX REPLACE "(.*)author=([^\n]+)(.*)" "\\2" plugin_author "${ini_text}")
+ read_ini_key(${ini_text} "title" plugin_title)
+ read_ini_key(${ini_text} "description" plugin_description)
+ read_ini_key(${ini_text} "version" plugin_version)
+ read_ini_key(${ini_text} "author" plugin_author)
+ #STRING(REGEX REPLACE "(.*)title=([^\n]+)(.*)" "\\2" plugin_title "${ini_text}")
+ #STRING(REGEX REPLACE "(.*)description=([^\n]+)(.*)" "\\2" description "${ini_text}")
+ #STRING(REGEX REPLACE "(.*)version=([^\n]+)(.*)" "\\2" plugin_version "${ini_text}")
+ #STRING(REGEX REPLACE "(.*)author=([^\n]+)(.*)" "\\2" plugin_author "${ini_text}")
 
  #message(${plugin_title})
  #message(${description})
